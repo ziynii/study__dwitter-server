e54b0f1c17c4f46b2989c9872f62eb989596bdb1
Jiyeon <ziyeon.ivi@gmail.com>
2023-05-09 04:49:51 +0900

    ~ socket

---
+++ data/auth.js
@@ -0,0 +1,32 @@
+// abcd1234: $2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm
+let users = [
+  {
+    id: '1',
+    username: 'bob',
+    password: '$2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm',
+    name: 'Bob',
+    email: 'bob@gmail.com',
+    url: 'https://images.unsplash.com/photo-1490750967868-88aa4486c946?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+  },
+  {
+    id: '2',
+    username: 'ellie',
+    password: '$2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm',
+    name: 'Ellie',
+    email: 'ellie@gmail.com',
+  },
+];
+
+export async function findByUsername(username) {
+  return users.find((user) => user.username === username);
+}
+
+export async function findById(id) {
+  return users.find((user) => user.id === id);
+}
+
+export async function createUser(user) {
+  const created = { ...user, id: Date.now().toString() };
+  users.push(created);
+  return created.id;
+}
